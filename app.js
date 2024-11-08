f3347110b0d6679ea0266969d02df9cbbcc591a8
minJonRing <1014080407tqr@gmail.com>
2024-11-08 17:12:57 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+// 启动数据库
+const statrDB = require('./models/index');
+statrDB()
+
+// 路由
+const index = require('./routes/index')
+const UserController = require("./controller/UserController")
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes: ['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  map: { html: 'ejs' }
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(UserController.routes(), UserController.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
