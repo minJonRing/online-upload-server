14f65fac32c412f56f43b36f64d7bf6b9513eb12
minJonRing <1014080407tqr@gmail.com>
2024-11-11 15:00:11 +0800

    123

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,8 @@ router.get('/uploadToConfigParam', async (ctx, next) => {
       version: 'v1.1',
       build: '/upload/build/build.zip',
       renderer: '/upload/renderer/renderer.zip',
-      dist: '/upload/static/static-152365.zip',
-      views: '/upload/views/index-152365.zip'
+      dist: '/upload/static/static.zip',
+      views: '/upload/views/index.zip'
     }
   }
 })
